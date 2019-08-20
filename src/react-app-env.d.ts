405e6ccfeef1c58e8cabed88357b4f1e362e9aaf
jay.liu <jay.liu@3ti.us>
2019-08-20 13:53:36 +0800

    压缩背景图片

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+declare module '*.svg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.jpg';
+declare module '*.jpeg';
+
+declare module '*.less';
+declare module '*.tsx';
+declare module '*.ts';
+
+declare module 'react-document-title';
